8e8392446fe98cf5483f2bd232297789cf186fba
FinnBoy <finn0918@163.com>
2019-06-23 15:36:06 +0800

    add sample

---
+++ src/main/java/finn/sample/web/controller/HomeController.java
@@ -0,0 +1,13 @@
+package finn.sample.web.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @RequestMapping("/")
+    public String home() {
+        return "Hello world!";
+    }
+}
